38f435b32ad545b0f23b41ace9e2f40ccce125fb
Onur Yıldırım <onury@users.noreply.github.com>
2026-05-26 04:13:35 +0300

    Add files via upload

--- codenames/sw.js
+++ codenames/sw.js
@@ -1,6 +1,6 @@
 // Codenames TR - Service Worker
 // Cache app shell so it works offline after first load
-const CACHE = 'codenames-v18';
+const CACHE = 'codenames-v21';
 const ASSETS = [
   './',
   './index.html',
